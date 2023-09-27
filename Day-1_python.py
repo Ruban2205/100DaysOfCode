25cd41295a30a1362ad5939a932c660625b37c91
Ruban Gino Singh <rubangino22@gmail.com>
2023-09-27 23:28:34 +0530

    Initial commit | ADD Day01 file

---
+++ Day-1_python.py
@@ -0,0 +1,11 @@
+# Day - 1 of #100DaysOfCode
+# Ruban Gino Singh - www.rubangino.in
+
+print("Ruban Gino Singh - Day1 of #100DaysOfCode")
+
+print("\nArea of Triangle\n")
+
+base = int(input("Enter the base: "))
+height = int(input("Enter the height: "))
+area = 1/2 * base * height
+print("Area of the trianle is: ", area)
