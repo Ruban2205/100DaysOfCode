5e1273e3e49ab5961b27c6c884b12d0360656940
Ruban Gino Singh <rubangino22@gmail.com>
2023-09-28 16:44:07 +0530

    ADD assets folder | ADD Day2 file

---
+++ Day-2_python.py
@@ -0,0 +1,17 @@
+# Program to find the largest of three numbers
+
+print("\nRuban Gino Singh - Day2 of #100DaysOfCode")
+
+print("\nFinding largest number!\n")
+
+num1 = int(input("Enter the number1: "))
+num2 = int(input("Enter the number2: "))
+num3 = int(input("Enter the number3: "))
+
+if num1 >= num2 and num1 >= num3: 
+    print(num1, " is greater!")
+elif num2 >= num1 and num2 >= num3: 
+    print(num2, " is greater!")
+else: 
+    print(num3, " is greater!")
+    
