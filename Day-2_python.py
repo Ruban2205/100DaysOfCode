a678d084193aa9fc48bb89b286c7a6625f04ee35
Ruban Gino Singh <rubangino22@gmail.com>
2023-09-28 17:09:19 +0530

    UPDATE Day2

--- Day-2_python.py
+++ Day-2_python.py
@@ -1,5 +1,4 @@
 # Program to find the largest of three numbers
-
 print("\nRuban Gino Singh - Day2 of #100DaysOfCode")
 
 print("\nFinding largest number!\n")
@@ -14,4 +13,17 @@ elif num2 >= num1 and num2 >= num3:
     print(num2, " is greater!")
 else: 
     print(num3, " is greater!")
-    
+
+
+# Calculate the factorial of a number using loop 
+print("\nRuban Gino Singh - Day2 of #100DaysOfCode")
+
+print("\nFactorial of a python number!\n")
+
+userInput = int(input("Enter a number: "))
+factorial = 1 
+
+if userInput >= 1: 
+    for i in range(1, userInput+1): 
+        factorial = factorial * i
+print("Factorial of the given number is: ", factorial)
