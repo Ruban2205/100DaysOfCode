24fad13bfa7de1e4dd5a3292c10fd2b01873125b
Ruban Gino Singh <rubangino22@gmail.com>
2023-12-26 22:16:45 +0530

    ADD day 91 file

---
+++ Day-91_python.py
@@ -0,0 +1,69 @@
+# Implement Kruskal's algorithm
+print("\nRuban Gino Singh - Day 91 of #100DaysOfCode\n")
+
+print("Python program to Implement a Kruskal's Algorithm.\n")
+ 
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    def union(self, parent, rank, x, y):
+        root_x = self.find(parent, x)
+        root_y = self.find(parent, y)
+
+        if rank[root_x] < rank[root_y]:
+            parent[root_x] = root_y
+        elif rank[root_x] > rank[root_y]:
+            parent[root_y] = root_x
+        else:
+            parent[root_x] = root_y
+            rank[root_y] += 1
+
+    def kruskal(self):
+        result = []
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+
+        parent = []
+        rank = []
+
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+
+        i = 0
+        e = 0
+
+        while e < self.V - 1:
+            u, v, w = self.graph[i]
+            i += 1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+
+            if x != y:
+                e += 1
+                result.append([u, v, w])
+                self.union(parent, rank, x, y)
+
+        print("Minimum Spanning Tree:")
+        for u, v, w in result:
+            print(f"{u} -- {v} == {w}")
+
+g = Graph(4)
+g.add_edge(0, 1, 10)
+g.add_edge(0, 2, 6)
+g.add_edge(0, 3, 5)
+g.add_edge(1, 3, 15)
+g.add_edge(2, 3, 4)
+
+g.kruskal()
+
+# --------------------------------------------------------- #
