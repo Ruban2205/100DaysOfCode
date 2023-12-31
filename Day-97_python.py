c392fe2c4a0fc0e6eb4997f531992dacefeb510a
Ruban Gino Singh <rubangino22@gmail.com>
2024-01-01 02:56:42 +0530

    ADD Day 97 file

---
+++ Day-97_python.py
@@ -0,0 +1,37 @@
+# Implementation of a Knapsack problem 
+print("\nRuban Gino Singh - Day 97 of #100DaysOfCode\n")
+
+print("Python Program to Implement a knapsack problem.\n")
+
+def knapsack(weights, values, capacity):
+    n = len(weights)
+
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(1, capacity + 1):
+            if weights[i - 1] <= w:
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
+            else:
+                dp[i][w] = dp[i - 1][w]
+
+    selected_items = []
+    i, j = n, capacity
+    while i > 0 and j > 0:
+        if dp[i][j] != dp[i - 1][j]:
+            selected_items.append(i - 1)
+            j -= weights[i - 1]
+        i -= 1
+
+    return dp[n][capacity], selected_items[::-1]
+
+weights = [2, 3, 4, 5]
+values = [3, 4, 5, 6]
+knapsack_capacity = 8
+
+max_value, selected_items = knapsack(weights, values, knapsack_capacity)
+
+print(f"Maximum value: {max_value}")
+print(f"Selected items: {selected_items}")
+
+# --------------------------------------------------------- #
