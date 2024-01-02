b6645fea760f35151fc437f7540a5fc5062f5e29
Ruban Gino Singh <rubangino22@gmail.com>
2024-01-02 12:03:52 +0530

    ADD day 98 file

---
+++ Day-98_python.py
@@ -0,0 +1,43 @@
+# Implement a Python program to perform a linked list 
+print("\nRuban Gino Singh - Day 98 of #100DaysOfCode\n")
+
+print("Python program to Implement a Linked List\n")
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    
+    linked_list.append(1)
+    linked_list.append(2)
+    linked_list.append(3)
+    linked_list.append(4)
+
+    print("Linked List:")
+    linked_list.display()
+
+# --------------------------------------------------------- #
